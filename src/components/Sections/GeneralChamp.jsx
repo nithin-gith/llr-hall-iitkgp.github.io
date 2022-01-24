97c02a1cb51a734315e7e1756e4b76c8d3d8633d
Nithin Siriguppa <nithin.siriguppa@gmail.com>
2022-01-25 01:17:43 +0530

    made improvements on GC page

--- src/components/Sections/GeneralChamp.jsx
+++ src/components/Sections/GeneralChamp.jsx
@@ -58,136 +58,115 @@ export default function BasicTabs() {
     return (
         <>
             
-                        <div className="head">
-                
+          <div className="head">
                 <h2 className="text">Best Performances in General ChampionShip</h2>
-            </div>
+          </div>
+          <div className="box" style ={{marginTop:"3rem"}}>
+            <Box sx={{ width: '100%',marginTop:"0px" }}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered>
+                  <Tab label="Technology" {...a11yProps(0)} />
+                  <Tab label="Sports and Games" {...a11yProps(1)} />
+                  <Tab label="Social and Cultural" {...a11yProps(2)} />
+                </Tabs>
+              </Box>
 
-            <div className="box">
-                
 
-     
-      
-      
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered>
-          <Tab label="Technology" {...a11yProps(0)} />
-          <Tab label="Sports and Games" {...a11yProps(1)} />
-          <Tab label="Social and Cultural" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-                      <div className="wrap">
-
-        
-            <div className="about-wrapper">
-            
-
+              
+                             
+<TabPanel value={value} index={0}>              
+  <div className="wrap">           
+     <div className="about-wrapper" >
         <div className="about">
+          <Fade left>
+          <div className="about-image" >
+            <img src={product} alt="product" />
+          </div>
+          </Fade>
+
+          <Fade right>
+          <div className="about-text">
+          <h1 className="heading-about">Gold in Product Design</h1>
+          <hr className="rule-about"/>
+          <i>"After 10 years and a lot of hard work we got this GOLD in ProductDesign"</i>
+          </div>
+          </Fade>
+        </div>
+      </div>
+
+      <div className="about-wrapper">
+        <div className="about">  
+          <Fade left>
+            <div className="about-image" >
+              <img src={casestudy} alt="casestudy" />
+            </div>
+          </Fade>
+
+
+          <Fade right>
+            <div className="about-text">
+              <h1 className="heading-about">Silver in Inter-Hall Case Study</h1>
+              <hr className="rule-about"/>
+              <i>"The team consisted of
+                  Sidhant Agarwal , Sagar Gupta,
+                  Anmol Thakkar , Bhavya Jain, Shashank Mishra , Suyash Sharma , Gaurav Katam ,
+                  Puneet Pansari , Ayush Vajpeyi , Aditya Neema, Agnimitra."</i>
+            </div>
+          </Fade>
+
+          
+        </div>
+      </div>
+  </div>
+</TabPanel>
+
+<TabPanel value={value} index={1}>
+  <div className="wrap">
+    <div className="about-wrapper">
+      <div className="about">
         <Fade left>
-        <div className="about-image">
-          <img src={product} alt="product" />
-        </div>
-        </Fade>
-<Fade right>
-
-        <div className="about-text">
-        <h1 className="heading-about">Gold in Product Design</h1>
-        <hr className="rule-about"/>
-        <i>"After 10 years and a lot of hard work we got this GOLD in ProductDesign"</i>
-        </div>
-</Fade>
-
-        </div>
-                            </div>
-                            <div className="about-wrapper">
-        
-
-        <div className="about">
-       
-<Fade left>
-
-        <div className="about-text">
-        <h1 className="heading-about">Silver in Inter-Hall Case Study</h1>
-        <hr className="rule-about"/>
-        <i>"The team consisted of
-Sidhant Agarwal , Sagar Gupta,
-Anmol Thakkar , Bhavya Jain, Shashank Mishra , Suyash Sharma , Gaurav Katam ,
-Puneet Pansari , Ayush Vajpeyi , Aditya Neema, Agnimitra."</i>
-        </div>
-</Fade>
-
-                     <Fade right>
-        <div className="about-image-right">
-          <img src={casestudy} alt="casestudy" />
-        </div>
-        </Fade>
-        </div>
+            <div className="about-image">
+              <img src={badminton} alt="LOGO" />
             </div>
-
-</div>
-                        
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-                        <div className="wrap">
-                            
-
-     
-                <div className="about-wrapper">
-        
-
-        <div className="about">
-        <Fade left>
-        <div className="about-image">
-          <img src={badminton} alt="LOGO" />
-        </div>
         </Fade>
-<Fade right>
 
-        <div className="about-text">
-        <h1 className="heading-about">Silver in Inter-Hall Badminton</h1>
-        <hr className="rule-about"/>
-        <i>"The team consisted of Akhilesh Gundu , Abhishek Velugu , Sandeep ."</i>
-        </div>
-</Fade>
-
-        </div>
+        <Fade right>
+            <div className="about-text">
+            <h1 className="heading-about">Silver in Inter-Hall Badminton</h1>
+            <hr className="rule-about"/>
+            <i>"The team consisted of Akhilesh Gundu , Abhishek Velugu , Sandeep ."</i>
             </div>
-               </div>
-
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-          <div className="wrap">
-                            
-
-     
-                <div className="about-wrapper">
-        
-
-        <div className="about">
-        <Fade left>
-        <div className="about-image">
-          <img src={play} alt="play" />
-        </div>
         </Fade>
-<Fade right>
+      </div>
+    </div>
+  </div>
+</TabPanel>
 
-        <div className="about-text">
-        <h1 className="heading-about">Gold in Inter-Hall Stage-Play</h1>
-        <hr className="rule-about"/>
-        <i>"The team consisted of : Atul Kumar , Shashi Kant , Somesh Gupta , Rohit Saboo ,  Umang Agarwal , Saurabh Pandey , Sayan Mandal ,  Rishabh Gupta , Shashank Jaipuria , Shyam Dixit , Abhishek kumar,Karan,Neel Ghosh,Prem Shankar,Ayanava Das,Souranshu Ghosh.
-Suyash Sharma , Apurv Dave helped in Direction.
-General Secretary Social & Cultural - Shubham Kumar
-Abhishek Yadav , Sushil Bagate , Abhijeet Mahato , Chandraprakash Koshle , @Utkarsh kumar helped in Sets and Sound.
-Along with these, There were some people who weren't in the team. Not even in the campus. Our Ex - Drams Captains who always helped us in the event. Abhay Daga , Shubham Singh"</i>
-        </div>
-</Fade>
+<TabPanel value={value} index={2}>
+      <div className="wrap">
+        <div className="about-wrapper">
+          <div className="about">
+            <Fade left>
+              <div className="about-image">
+                <img src={play} alt="play" />
+              </div>
+            </Fade>
 
+            <Fade right>
+              <div className="about-text">
+                <h1 className="heading-about">Gold in Inter-Hall Stage-Play</h1>
+                <hr className="rule-about"/>
+                <i>"The team consisted of : Atul Kumar , Shashi Kant , Somesh Gupta , Rohit Saboo ,  Umang Agarwal , Saurabh Pandey , Sayan Mandal ,  Rishabh Gupta , Shashank Jaipuria , Shyam Dixit , Abhishek kumar,Karan,Neel Ghosh,Prem Shankar,Ayanava Das,Souranshu Ghosh.
+                  Suyash Sharma , Apurv Dave helped in Direction.
+                  General Secretary Social & Cultural - Shubham Kumar
+                  Abhishek Yadav , Sushil Bagate , Abhijeet Mahato , Chandraprakash Koshle , @Utkarsh kumar helped in Sets and Sound.
+                  Along with these, There were some people who weren't in the team. Not even in the campus. Our Ex - Drams Captains who always helped us in the event. Abhay Daga , Shubham Singh"</i>
+              </div>
+            </Fade>
+          </div>
         </div>
-            </div>
-               </div>
-      </TabPanel>
+      </div>
+</TabPanel>
                 </Box>
                    </div>
             </>

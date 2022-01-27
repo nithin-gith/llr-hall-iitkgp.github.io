a65d660163c4d30172332cbdf40e0c7b3daaeb28
adityaag121 <adityaag121@gmail.com>
2022-01-28 02:49:32 +0530

    fixed album thumbnails

--- src/screens/Album.jsx
+++ src/screens/Album.jsx
@@ -2,50 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { albums } from "../assets/gallery";
 
-import Card from "@mui/material/Card"
-import { CardMedia, Grid, Typography ,CardContent} from "@mui/material";
+import "../style/album.css";
+
+import Card from "@mui/material/Card";
+import { CardMedia, Grid, Typography, CardContent } from "@mui/material";
 import { height, maxHeight, minHeight, width } from "@mui/system";
 const Album = () => (
   <>
-      <div className="head">
-          <h2 className="text">Gallery</h2>
-        </div>
+    <div className="head">
+      <h2 className="text">Gallery</h2>
+    </div>
     <Grid container columnGap={2} justifyContent="center">
-    {Object.entries(albums).map(([key, album]) => (
-      <Link to={key}>
- 
-        <Grid item style={{
-         
-          width : "100%",
-       
-        
-        }}>
-          <Card xs={12} sm={6} md={4} sx={{
-            maxWidth:
-              475,
-            
-             
-              }}  >
-            <CardMedia component="img" image={album.thumbnail}
+      {Object.entries(albums).map(([key, album]) => (
+        <Link to={key}>
+          <Grid
+            item
+            style={{
+              width: "100%",
+            }}
+          >
+            <Card
+              xs={12}
+              sm={6}
+              md={4}
               sx={{
-            
-           
+                maxWidth: 475,
               }}
-              style={{
-                marginTop : '2%',
-            
-             
-              }}
-          />
-            <CardContent><div className="card-title" align=
-         'center'>{album.title}</div></CardContent> 
-          </Card>
-            
-         </Grid>   
-      </Link>
-    ))}
-      
-      </Grid>
+            >
+              <CardMedia
+                component="img"
+                image={album.thumbnail}
+                sx={{}}
+                style={{
+                  marginTop: "2%",
+                }}
+                classes={{ img: "album-thumbnail" }}
+              />
+              <CardContent>
+                <div className="card-title" align="center">
+                  {album.title}
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Link>
+      ))}
+    </Grid>
   </>
 );
 

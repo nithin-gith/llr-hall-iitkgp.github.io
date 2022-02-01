8aa1f4a420bd0a5c9daf3d31d19e004ff999c923
adityaag121 <adityaag121@gmail.com>
2022-02-01 16:52:38 +0530

    about us updated

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -3,33 +3,39 @@ import logo from '../../assets/img/Hall.jfif'
 import '../../style/About.css'
 import Fade from 'react-reveal/Fade';
 const About = () => {
-    return( <div>
-      
-         <div className="about-wrapper">
-        
-
-        <div className="about">
-        <Fade left>
-        <div className="about-image">
-          <img src={logo} alt="LOGO" />
+    return (
+      <div>
+        <div className="about-wrapper">
+          <div className="about">
+            <Fade left>
+              <div className="about-image">
+                <img src={logo} alt="LOGO" />
+              </div>
+            </Fade>
+            <Fade right>
+              <div className="about-text" style={{ width: "90%" }}>
+                <h1 className="heading-about">About Us</h1>
+                <hr className="rule-about" />
+                <p>
+                  <i>
+                    The hall was founded on March 18, 1967, and the name was
+                    given in honour of the great freedom fighter and leader of
+                    Indian Independence Movement, Lala Lajpat Rai.
+                  </i>
+                </p>
+                <br />
+                <p>
+                  <i>
+                    We proudly stand by our Motto, <b>"कर्मणैव हि संसिद्धि"</b>,
+                    which means "Perfection is attained by performing duties".
+                  </i>
+                </p>
+              </div>
+            </Fade>
+          </div>
         </div>
-        </Fade>
-<Fade right>
-
-        <div className="about-text" style={{width:"90%"}}>
-        <h1 className="heading-about">About Us</h1>
-        <hr className="rule-about"/>
-        <i>The hall was founded on March 18, 1967, and the name was given in honour of the great freedom fighter and leader of Indian Independence Movement, Lala Lajpat Rai.</i>
-        </div>
-</Fade>
-
-        </div>
-        </div>
-
-
-    </div>
-    
-    )
+      </div>
+    );
 };
 
 export default About;

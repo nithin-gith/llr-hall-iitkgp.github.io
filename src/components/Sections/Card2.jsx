6076f77d69b6283b28c02048a088c94b85b2cf3c
Krishna Chaudhari <krishnachaudhari2309@gmail.com>
2024-01-06 08:30:48 +0530

    krishna-hall council and our team

--- src/components/Sections/Card2.jsx
+++ src/components/Sections/Card2.jsx
@@ -31,23 +31,27 @@ const Card = (props) => {
 
     };
 
-const figure = document.querySelectorAll('figure');
-const caption = document.querySelectorAll('figcaption');
-const fullCaption = document.querySelectorAll('.caption-full');
-const figureImage = document.querySelectorAll('figure img');
+// const figure = document.querySelectorAll('figure');
+// const caption = document.querySelectorAll('figcaption');
+// const fullCaption = document.querySelectorAll('.caption-full');
+// const figureImage = document.querySelectorAll('figure img');
+// const captiontitle = document.querySelectorAll('.caption-title');
 
-figure.forEach((fig, idx) => {
-    fig.addEventListener('mouseover', () => {
-        caption[idx].classList.add('expanded');
-        fullCaption[idx].classList.add('visible');
-        figureImage[idx].classList.add('image-filter');
-    });
+
+// figure.forEach((fig, idx) => {
+//     fig.addEventListener('mouseover', () => {
+//         caption[idx].classList.add('expanded');
+//         captiontitle[idx].classList.add('hidetitle')
+//         fullCaption[idx].classList.add('visible');
+//         figureImage[idx].classList.add('image-filter');
+//     });
     
-    fig.addEventListener('mouseout', () => {
-       caption[idx].classList.remove('expanded');
-       fullCaption[idx].classList.remove('visible');
-       figureImage[idx].classList.remove('image-filter');
-    });
-});
+//     fig.addEventListener('mouseout', () => {
+//         caption[idx].classList.remove('expanded');
+//         captiontitle[idx].classList.remove('hidetitle')
+//         fullCaption[idx].classList.remove('visible');
+//         figureImage[idx].classList.remove('image-filter');
+//     });
+// });
 
 export default Card;
